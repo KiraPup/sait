25a6c6bc0cde5cafa42511c608d8bc75bb2656bb
Кира <gruppa3031@gmail.com>
2025-07-28 15:16:11 +0300

    Создано окно и виджеты

--- sait.py
+++ sait.py
@@ -1,1 +1,24 @@
 from tkinter import *
+from PIL import Image, ImageTk #работа с картинками
+import requests
+from io import BytesIO #работа с двоичной системой
+
+
+window = Tk()
+window.title('Cats!')
+window.geometry('600x480')
+
+#создаем местку. где будет изображение
+label=Label()
+label.pack()
+
+#адрес из интернета, из которого берем информацию
+url = "https://cataas.com/cat"
+img = load_image(url) #сделать загрузку изображения
+
+#проверка если не пустая переменная,
+if img:
+    label.config(image=img)
+    label.image = img #если эту строчку не написать, то комп ее выдаст, а так какмусор удалтит
+
+window.mainloop()
